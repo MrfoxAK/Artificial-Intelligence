e26db9f5b7d83141794625c1072f8b4b36c868ec
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2024-03-26 20:37:09 +0530

    Delete Deep Learning/Natural language processing (NLP) directory

--- Deep Learning/Natural language processing (NLP)/Text_Preprocessing/Handling_Emojis.py
+++
@@ -1,6 +0,0 @@
-text = "Hey! I'm so excited to see you later! 😁 Can't wait to catch up and hear all about your trip. ✈️ What time should I pick you up? 🚗"
-
-
-output = text.encode('utf-8')
-
-print(output)

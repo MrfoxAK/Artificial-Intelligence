27f8e1493acc8510917d83e554eab88d91cbb322
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2024-09-12 21:22:20 +0530

    Add files via upload

---
+++ Natural language processing (NLP)/Text_Preprocessing/Handling_Emojis.py
@@ -0,0 +1,6 @@
+text = "Hey! I'm so excited to see you later! 😁 Can't wait to catch up and hear all about your trip. ✈️ What time should I pick you up? 🚗"
+
+
+output = text.encode('utf-8')
+
+print(output)
